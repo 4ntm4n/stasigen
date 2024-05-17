b4253c21253be9189f25022588c5d8b7d3f86d0b
4ntm4n <anton.askling@gmail.com>
2024-05-18 01:15:09 +0200

    add textnode.py

---
+++ src/textnode.py
@@ -0,0 +1,16 @@
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+
+    def __eq__(self, other):
+        return   (
+            other.text == self.text
+        and other.text_type == self.text_type
+        and other.url == self.url
+        )
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
